e82bc2f153d752bc0d502ca5f7b57ef1696a3f07
“Mnishthakur” <mnish771@gmail.com>
2023-03-06 19:31:09 +0530

    Add: As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,8 +1,23 @@
-result=$((RANDOM % 2))
+# Set the number of times to flip the coin
+num_flips=10
 
-if [ $result -eq 0 ]
-then
-  echo "Heads"
-else
-  echo "Tails"
-fi
+# Initialize counters for heads and tails
+heads=0
+tails=0
+
+# Flip the coin and increment the corresponding counter
+for (( i=1; i<=$num_flips; i++ ))
+do
+  result=$((RANDOM % 2))
+  if [ $result -eq 0 ]
+  then
+    heads=$((heads+1))
+  else
+    tails=$((tails+1))
+  fi
+done
+
+# Display the results
+echo "Results after $num_flips flips:"
+echo "Heads: $heads"
+echo "Tails: $tails"
